db8ab3090d3f9622f6a75dc0fc0895bdf2658d6b
ronshukrun <136229151+ronshukrun@users.noreply.github.com>
2025-07-03 13:13:47 +0300

    final store.js

--- src/store.js
+++ src/store.js
@@ -1,22 +1,3 @@
-// import { reactive } from 'vue';
-
-// const store = reactive({
-//   username: localStorage.getItem('username'),
-//   server_domain: "http://localhost:3000",
-
-//   login(username) {
-//     localStorage.setItem('username', username);
-//     this.username = username;
-//     console.log("login", this.username);
-//   },
-
-//   logout() {
-//     console.log("logout");
-//     localStorage.removeItem('username');
-//     this.username = undefined;
-//   }
-// });
-
 // export default store;
 import { reactive } from 'vue';
 
